aaf3ea34b6af6a0ad06df3ab26a99783354c54f5
Johann Nunez Garcia <johann2357@gmail.com>
2021-04-03 23:12:37 -0500

    feat(nvim): improve hardline config

--- neovim/lua/johann2357/hardline.lua
+++ neovim/lua/johann2357/hardline.lua
@@ -1,4 +1,15 @@
 require("hardline").setup {
     bufferline = true,
-    theme = "gruvbox"
+    theme = "gruvbox",
+    sections = {
+        {class = 'mode', item = require('hardline.parts.mode').get_item},
+        {class = 'high', item = require('hardline.parts.git').get_item, hide = 80},
+        '%<',
+        {class = 'med', item = require('hardline.parts.filename').get_item},
+        {class = 'med', item ='%='},
+        -- {class = 'low', item = require('hardline.parts.wordcount').get_item, hide = 80},
+        {class = 'error', item = require('hardline.parts.lsp').get_error},
+        {class = 'warning', item = require('hardline.parts.lsp').get_warning},
+        {class = 'warning', item = require('hardline.parts.whitespace').get_item},
+    },
 }
