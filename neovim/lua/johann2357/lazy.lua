b043b45ceb5078493fd1e8fe90aa16c9a7a59e3b
Johann Nunez <johann2357@gmail.com>
2023-02-05 15:28:06 -0500

    chore: cleanup plugins

--- neovim/lua/johann2357/lazy.lua
+++ neovim/lua/johann2357/lazy.lua
@@ -1,76 +1,92 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-    -- Packer can manage itself
-    'wbthomason/packer.nvim',
-    -- tpope
-    'tpope/vim-fugitive',
-    'tpope/vim-rhubarb',
-    'tpope/vim-vinegar',
-    'tpope/vim-surround',
-    -- treesitter
+require('lazy').setup(
     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
+        -- tpope
+        'tpope/vim-fugitive',
+        'tpope/vim-rhubarb',
+        'tpope/vim-vinegar',
+        'tpope/vim-surround',
+        -- treesitter
+        {
+            'nvim-treesitter/nvim-treesitter',
+            build = ':TSUpdate'
+        },
+        'nvim-treesitter/playground',
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter-context',
+        -- LSP
+        {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v1.x',
+            dependencies = {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
+                -- Autocompletion
+                {'saadparwaiz1/cmp_luasnip'},
+                {'hrsh7th/nvim-cmp'},
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'hrsh7th/cmp-nvim-lua'},
+                {'hrsh7th/cmp-buffer'},
+                {'hrsh7th/cmp-path'},
+                -- Snippets
+                {'L3MON4D3/LuaSnip'},
+                {'rafamadriz/friendly-snippets'},
+            }
+        },
+        -- productivity
+        {
+            'nvim-telescope/telescope.nvim',
+            version = '0.1.1',
+            -- branch = '0.1.x',
+            dependencies = { {'nvim-lua/plenary.nvim'} }
+        },
+        'ThePrimeagen/git-worktree.nvim',
+        'ThePrimeagen/harpoon',
+        'numToStr/Comment.nvim',
+        'mbbill/undotree',
+        'folke/zen-mode.nvim',
+        -- look and feel
+        'nvim-tree/nvim-web-devicons',
+        {
+            'catppuccin/nvim',
+            name = 'catppuccin',
+            priority = 420,
+            lazy = false,
+        },
+        {
+            'johann2357/nvim-hardline',
+            branch = 'harpoon',
+        },
     },
-    'nvim-treesitter/playground',
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    'nvim-treesitter/nvim-treesitter-context',
-    -- LSP
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-            -- Autocompletion
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    },
-    -- productivity
-    {
-        'nvim-telescope/telescope.nvim',
-        version = '0.1.1',
-        -- branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
-    },
-    'ThePrimeagen/git-worktree.nvim',
-    'ThePrimeagen/harpoon',
-    'numToStr/Comment.nvim',
-    'mbbill/undotree',
-    'folke/zen-mode.nvim',
-    -- look and feel
-    'nvim-tree/nvim-web-devicons',
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 420,
-    },
-    {
-        'johann2357/nvim-hardline',
-        branch = 'harpoon',
-    },
-    -- fun
-    'eandrju/cellular-automaton.nvim',
-})
+        ui = {
+            icons = {
+                cmd = "⌘",
+                config = "🛠",
+                event = "📅",
+                ft = "📂",
+                init = "⚙",
+                keys = "🗝",
+                plugin = "🔌",
+                runtime = "💻",
+                source = "📄",
+                start = "🚀",
+                task = "📌",
+            },
+        },
+    }
+)
