f0867f1dfabe27abcd713c381bcde0ee0f924012
Johann Nunez <johann2357@gmail.com>
2023-04-03 23:27:15 -0500

    chore(neovim): improve lsp setup

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -36,6 +36,9 @@ local lsp_attach = function (_, bufnr)
 end
 
 cmp.setup({
+    view = {
+        entries = {name = "custom", selection_order = "top_down"},
+    },
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
@@ -70,16 +73,18 @@ cmp.setup({
         }
     ),
     formatting = {
+       fields = { "abbr", "kind" },
        format = lspkind.cmp_format({
-           mode = 'symbol_text', -- show only symbol annotations
-           maxwidth = 40, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-           ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+           mode = "symbol_text", -- show only symbol annotations
+           maxwidth = 30, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+           ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
            -- The function below will be called before any actual modifications from lspkind
            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-           -- before = function (entry, vim_item)
-           --   return vim_item
-           -- end
+           before = function (_, vim_item)
+               vim_item.menu = nil
+               return vim_item
+           end
        })
     },
 })
