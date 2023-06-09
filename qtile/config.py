c77a06890ad8d53fde0e31cce67a2e56db54e078
Johann Nunez <johann2357@gmail.com>
2023-06-09 08:19:51 -0500

    chore(qtile): split config into files

--- qtile/config.py
+++ qtile/config.py
@@ -1,253 +1,23 @@
 import subprocess
-from enum import Enum
 
 import iwlib
 import psutil
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.log_utils import logger
 from libqtile.utils import guess_terminal
 from libqtile.widget import base
 
-
-class Gruvbox(str, Enum):
-    dark0_hard = "#1d2021"  # 29-32-33
-    dark0 = "#282828"  # 40-40-40
-    dark0_soft = "#32302f"  # 50-48-47
-    dark1 = "#3c3836"  # 60-56-54
-    dark2 = "#504945"  # 80-73-69
-    dark3 = "#665c54"  # 102-92-84
-    dark4 = "#7c6f64"  # 124-111-100
-    dark4_256 = "#7c6f64"  # 124-111-100
-    gray_245 = "#928374"  # 146-131-116
-    gray_244 = "#928374"  # 146-131-116
-    gray = "#928374"  # 146-131-116
-    light0_hard = "#f9f5d7"  # 249-245-215
-    light0 = "#fbf1c7"  # 253-244-193
-    light0_soft = "#f2e5bc"  # 242-229-188
-    light1 = "#ebdbb2"  # 235-219-178
-    light2 = "#d5c4a1"  # 213-196-161
-    light3 = "#bdae93"  # 189-174-147
-    light4 = "#a89984"  # 168-153-132
-    light4_256 = "#a89984"  # 168-153-132
-    bright_red = "#fb4934"  # 251-73-52
-    bright_green = "#b8bb26"  # 184-187-38
-    bright_yellow = "#fabd2f"  # 250-189-47
-    bright_blue = "#83a598"  # 131-165-152
-    bright_purple = "#d3869b"  # 211-134-155
-    bright_aqua = "#8ec07c"  # 142-192-124
-    bright_orange = "#fe8019"  # 254-128-25
-    neutral_red = "#cc241d"  # 204-36-29
-    neutral_green = "#98971a"  # 152-151-26
-    neutral_yellow = "#d79921"  # 215-153-33
-    neutral_blue = "#458588"  # 69-133-136
-    neutral_purple = "#b16286"  # 177-98-134
-    neutral_aqua = "#689d6a"  # 104-157-106
-    neutral_orange = "#d65d0e"  # 214-93-14
-    faded_red = "#9d0006"  # 157-0-6
-    faded_green = "#79740e"  # 121-116-14
-    faded_yellow = "#b57614"  # 181-118-20
-    faded_blue = "#076678"  # 7-102-120
-    faded_purple = "#8f3f71"  # 143-63-113
-    faded_aqua = "#427b58"  # 66-123-88
-    faded_orange = "#af3a03"  # 175-58-3
-
+from settings.themes import Gruvbox
+from settings.shortcuts import setup_groups, setup_keys
 
 theme = Gruvbox
 mod = "mod4"
 terminal = guess_terminal()
 
-keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "n", lazy.layout.next(), desc="Move window focus to other window"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key(
-        [mod],
-        "comma",
-        lazy.layout.decrease_ratio(),
-        desc="Shrink master",
-    ),
-    Key(
-        [mod],
-        "period",
-        lazy.layout.increase_ratio(),
-        desc="Grow master",
-    ),
-    Key([mod], "l", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "h", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key(
-        [mod, "shift"],
-        "h",
-        lazy.layout.shuffle_left(),
-        desc="Move window to the left",
-    ),
-    Key(
-        [mod, "shift"],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key(
-        [mod, "shift"],
-        "j",
-        lazy.layout.shuffle_down(),
-        desc="Move window down",
-    ),
-    Key(
-        [mod, "shift"],
-        "k",
-        lazy.layout.shuffle_up(),
-        desc="Move window up",
-    ),
-    Key(
-        [mod, "shift"],
-        "r",
-        lazy.layout.normalize(),
-        desc="Reset all window sizes",
-    ),
-    Key(
-        [mod],
-        "w",
-        lazy.next_screen(),
-        desc="Move to next screen",
-    ),
-    Key(
-        [mod, "shift"],
-        "w",
-        lazy.prev_screen(),
-        desc="Move to prev screen",
-    ),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    # TODO: figure out what this is for
-    # Key(
-    #     [mod, "shift"],
-    #     "Return",
-    #     lazy.layout.toggle_split(),
-    #     desc="Toggle between split and unsplit sides of stack",
-    # ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    # MEDIA KEYS
-    #   - Volume
-    Key(
-        [],
-        "XF86AudioLowerVolume",
-        lazy.spawn("pamixer --decrease 5"),
-        desc="Lower Volume by 5%",
-    ),
-    Key(
-        [],
-        "XF86AudioRaiseVolume",
-        lazy.spawn("pamixer --increase 5"),
-        desc="Increase Volume by 5%",
-    ),
-    Key(
-        [],
-        "XF86AudioMute",
-        lazy.spawn("pamixer --toggle-mute"),
-        desc="Toggle Mute",
-    ),
-    #   - Brightness
-    Key(
-        [mod],
-        # "XF86MonBrightnessUp",
-        "F6",
-        lazy.spawn("brightnessctl set 5%+"),
-        desc="Increase Brightness by 5%",
-    ),
-    Key(
-        [mod],
-        # "XF86MonBrightnessDown",
-        "F5",
-        lazy.spawn("brightnessctl set 5%-"),
-        desc="Decrease Brightness by 5%",
-    ),
-    #   - Screenshot
-    Key(
-        [mod, "shift"],
-        "s",
-        lazy.spawn("sshot2clip"),
-        desc="Store screenshot in clipboard",
-    ),
-    Key(
-        [mod, "shift"],
-        "z",
-        lazy.spawn("sshot2file"),
-        desc="Store screenshot in ~/Screenshots dir",
-    ),
-]
-
-groups = [
-    Group(
-        name="1",
-        screen_affinity=0,
-        matches=[Match(title=["Brave"])],
-    ),
-    Group(
-        name="2",
-        screen_affinity=0,
-        matches=[Match(title=["Alacritty"])],
-    ),
-    Group(
-        name="3",
-        screen_affinity=0,
-        matches=[Match(title=["Google Chrome"])],
-    ),
-    Group(
-        name="4",
-        screen_affinity=0,
-    ),
-    Group(
-        name="5",
-        screen_affinity=1,
-    ),
-    Group(
-        name="6",
-        screen_affinity=1,
-        matches=[Match(title=["Slack"])],
-    ),
-]
-
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+groups = setup_groups()
+keys = setup_keys(mod=mod, terminal=terminal, groups=groups)
 
 layouts = [
     layout.Tile(
@@ -261,17 +31,6 @@ layouts = [
         expand=True,
     ),
     layout.Max(margin=[1, 1, 1, 1]),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
@@ -593,3 +352,6 @@ wl_input_rules = None
 # java that happens to be on java's whitelist.
 # wmname = "LG3D"
 wmname = "Qtile"
+
+print("Holis")
+logger.warning("Holis")
---
+++ qtile/settings/shortcuts.py
@@ -0,0 +1,205 @@
+from libqtile.config import Group, Key, Match
+from libqtile.lazy import lazy
+from libqtile.log_utils import logger
+
+
+def setup_groups() -> list[Group]:
+    logger.info("[keys] Setting up groups")
+    return [
+        Group(
+            name="1",
+            screen_affinity=0,
+            matches=[Match(title=["Brave"])],
+        ),
+        Group(
+            name="2",
+            screen_affinity=0,
+            matches=[Match(title=["Alacritty"])],
+        ),
+        Group(
+            name="3",
+            screen_affinity=0,
+            matches=[Match(title=["Google Chrome"])],
+        ),
+        Group(
+            name="4",
+            screen_affinity=0,
+        ),
+        Group(
+            name="5",
+            screen_affinity=1,
+        ),
+        Group(
+            name="6",
+            screen_affinity=1,
+            matches=[Match(title=["Slack"])],
+        ),
+    ]
+
+
+def setup_keys(
+    mod: str,
+    terminal: str,
+    groups: list[Group],
+) -> list[Key]:
+    logger.info("[keys] Setting up keys")
+    keys = [
+        # A list of available commands that can be bound to keys can be found
+        # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+        # Switch between windows
+        Key([mod], "n", lazy.layout.next(), desc="Move window focus to other window"),
+        # Grow windows. If current window is on the edge of screen and direction
+        # will be to screen edge - window would shrink.
+        Key(
+            [mod],
+            "comma",
+            lazy.layout.decrease_ratio(),
+            desc="Shrink master",
+        ),
+        Key(
+            [mod],
+            "period",
+            lazy.layout.increase_ratio(),
+            desc="Grow master",
+        ),
+        Key([mod], "l", lazy.layout.left(), desc="Move focus to left"),
+        Key([mod], "h", lazy.layout.right(), desc="Move focus to right"),
+        Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+        Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+        # Move windows between left/right columns or move up/down in current stack.
+        # Moving out of range in Columns layout will create new column.
+        Key(
+            [mod, "shift"],
+            "h",
+            lazy.layout.shuffle_left(),
+            desc="Move window to the left",
+        ),
+        Key(
+            [mod, "shift"],
+            "l",
+            lazy.layout.shuffle_right(),
+            desc="Move window to the right",
+        ),
+        Key(
+            [mod, "shift"],
+            "j",
+            lazy.layout.shuffle_down(),
+            desc="Move window down",
+        ),
+        Key(
+            [mod, "shift"],
+            "k",
+            lazy.layout.shuffle_up(),
+            desc="Move window up",
+        ),
+        Key(
+            [mod, "shift"],
+            "r",
+            lazy.layout.normalize(),
+            desc="Reset all window sizes",
+        ),
+        Key(
+            [mod],
+            "w",
+            lazy.next_screen(),
+            desc="Move to next screen",
+        ),
+        Key(
+            [mod, "shift"],
+            "w",
+            lazy.prev_screen(),
+            desc="Move to prev screen",
+        ),
+
+        # Toggle between split and unsplit sides of stack.
+        # Split = all windows displayed
+        # Unsplit = 1 window displayed, like Max layout, but still with
+        # multiple stack panes
+        # TODO: figure out what this is for
+        # Key(
+        #     [mod, "shift"],
+        #     "Return",
+        #     lazy.layout.toggle_split(),
+        #     desc="Toggle between split and unsplit sides of stack",
+        # ),
+        Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+        # Toggle between different layouts as defined below
+        Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+        Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+        Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+        Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+        Key([mod], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+        # MEDIA KEYS
+        #   - Volume
+        Key(
+            [],
+            "XF86AudioLowerVolume",
+            lazy.spawn("pamixer --decrease 5"),
+            desc="Lower Volume by 5%",
+        ),
+        Key(
+            [],
+            "XF86AudioRaiseVolume",
+            lazy.spawn("pamixer --increase 5"),
+            desc="Increase Volume by 5%",
+        ),
+        Key(
+            [],
+            "XF86AudioMute",
+            lazy.spawn("pamixer --toggle-mute"),
+            desc="Toggle Mute",
+        ),
+        #   - Brightness
+        Key(
+            [mod],
+            # "XF86MonBrightnessUp",
+            "F6",
+            lazy.spawn("brightnessctl set 5%+"),
+            desc="Increase Brightness by 5%",
+        ),
+        Key(
+            [mod],
+            # "XF86MonBrightnessDown",
+            "F5",
+            lazy.spawn("brightnessctl set 5%-"),
+            desc="Decrease Brightness by 5%",
+        ),
+        #   - Screenshot
+        Key(
+            [mod, "shift"],
+            "s",
+            lazy.spawn("sshot2clip"),
+            desc="Store screenshot in clipboard",
+        ),
+        Key(
+            [mod, "shift"],
+            "z",
+            lazy.spawn("sshot2file"),
+            desc="Store screenshot in ~/Screenshots dir",
+        ),
+    ]
+    for i in groups:
+        keys.extend(
+            [
+                # mod1 + letter of group = switch to group
+                Key(
+                    [mod],
+                    i.name,
+                    lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name),
+                ),
+                # mod1 + shift + letter of group = switch to & move focused window to group
+                Key(
+                    [mod, "shift"],
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(i.name),
+                ),
+                # Or, use below if you prefer not to switch to that group.
+                # # mod1 + shift + letter of group = move focused window to group
+                # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+                #     desc="move focused window to group {}".format(i.name)),
+            ]
+        )
+
+    return keys
