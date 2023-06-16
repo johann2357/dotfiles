cda1918ec6dd5914b8c951a8c0bd1d8db1b73a14
Johann Nunez <johann2357@gmail.com>
2023-06-16 10:30:29 -0500

    chore(qtile): improve bar widgets
    add cpu custom widget
    improve wlan widget

--- qtile/settings/bar.py
+++ qtile/settings/bar.py
@@ -142,13 +142,13 @@ def get_icon_wlan(status: WlanInfo):
         return "network-wireless-disconnected"
 
     quality = status.get_quality_percentage()
-    if quality < 0.1:
+    if quality < 0.10:
         return "network-wireless-connected-00"
-    elif quality < 0.25:
+    elif quality < 0.30:
         return "network-wireless-connected-25"
-    elif quality < 0.5:
+    elif quality < 0.60:
         return "network-wireless-connected-50"
-    elif quality < 0.75:
+    elif quality < 0.90:
         return "network-wireless-connected-75"
     else:
         return "network-wireless-connected-100"
@@ -251,6 +251,49 @@ class VolumeIcon(widget.PulseVolume):
             self.draw()
 
 
+@dataclasses.dataclass
+class CPUInfo:
+    load_percent: float
+    current_freq: float
+    max_freq: float
+    min_freq: float
+
+
+def get_status_cpu() -> CPUInfo:
+    # cpu_usage = psutil.cpu_percent(interval=1)
+    load_percent = round(psutil.cpu_percent(), 1)
+    freq = psutil.cpu_freq()
+    freq_current = round(freq.current / 1000, 1)
+    freq_max = round(freq.max / 1000, 1)
+    freq_min = round(freq.min / 1000, 1)
+
+    return CPUInfo(
+        load_percent=load_percent,
+        current_freq=freq_current,
+        max_freq=freq_max,
+        min_freq=freq_min,
+    )
+
+
+def get_icon_cpu(status: CPUInfo):
+    logger.warning("[cpu] %s", str(status))
+
+    if status.load_percent < 25:
+        return "indicator-cpufreq-25"
+    elif status.load_percent < 50:
+        return "indicator-cpufreq-50"
+    elif status.load_percent < 75:
+        return "indicator-cpufreq-75"
+    else:
+        return "indicator-cpufreq-100"
+
+
+class CPUTextBox(ShowHideTextBox):
+    def calculate_text(self) -> str:
+        status = get_status_cpu()
+        return f"{status.current_freq}/{status.max_freq} GHz {status.load_percent}% "
+
+
 class BatteryTextBox(ShowHideTextBox):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -285,7 +328,7 @@ class MemoryTextBox(ShowHideTextBox):
         }
         self.measure_mem = "G"
         self.calc_mem = self.measures[self.measure_mem]
-        self.format = "{MemUsed:.2f} / {MemTotal:.2f} {mm} "
+        self.format = "{MemUsed:.2f}/{MemTotal:.2f} {mm}B "
 
     def calculate_text(self) -> str:
         mem = psutil.virtual_memory()
@@ -356,10 +399,32 @@ def create_bar(theme) -> bar.Bar:
                 update_interval=60,
             ),
             BatteryTextBox(),
+            IconWidget(
+                icon_status=IconStatus(
+                    icon_names=[
+                        "indicator-cpufreq",
+                        "indicator-cpufreq-25",
+                        "indicator-cpufreq-50",
+                        "indicator-cpufreq-75",
+                        "indicator-cpufreq-100",
+                    ],
+                    current_icon="indicator-cpufreq",
+                    get_status_function=get_status_cpu,
+                    get_icon_function=get_icon_cpu,
+                    theme_path="/usr/share/icons/Papirus/24x24/panel/",
+                ),
+            ),
+            CPUTextBox(),
             widget.Image(
                 filename="/usr/share/icons/Papirus/24x24/panel/indicator-sensors-memory.svg",
             ),
             MemoryTextBox(),
+            # widget.Image(
+            #     filename="/usr/share/icons/Papirus/24x24/panel/psensor_normal.svg",
+            # ),
+            # widget.ThermalSensor(
+            #     format="{temp:.1f}{unit}",
+            # ),
             IconWidget(
                 icon_status=IconStatus(
                     icon_names=[
@@ -389,17 +454,5 @@ def create_bar(theme) -> bar.Bar:
             widget.Clock(
                 format=" %a %d %b %H:%M",
             ),
-            # widget.Image(
-            #     filename="/usr/share/icons/Papirus/24x24/panel/indicator-cpufreq-25.svg",
-            # ),
-            # widget.CPU(
-            #     format="{load_percent}% ",
-            # ),
-            # widget.Image(
-            #     filename="/usr/share/icons/Papirus/24x24/panel/psensor_normal.svg",
-            # ),
-            # widget.ThermalSensor(
-            #     format="{temp:.1f}{unit}",
-            # ),
         ],
     )
