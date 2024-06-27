4ba55b0f875dd98a26a61351867d62f9ad5b38f9
Johann Nunez <johann2357@gmail.com>
2024-06-26 20:06:50 -0500

    chore(neovim): update deprecation warnings

--- neovim/after/plugin/gitsigns.lua
+++ neovim/after/plugin/gitsigns.lua
@@ -11,14 +11,6 @@ set_highlight("GitSignsChange", "#897d73")
 set_highlight("GitSignsDelete", "#8a4f5b")
 
 require("gitsigns").setup {
-    signs = {
-        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr" },
-        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-        changedelete = { hl = "GitSignsDelete", text = "~", numhl = "GitSignsChangeNr" },
-    },
-
     -- Highlights just the number part of the number column
     numhl = true,
 
